b27b48ccbaa2fa03a2cebe5f0011d28e1708a113
Julian Rosse <julian@helixbass.net>
2019-11-19 16:24:03 -0700

    reconcile v0.0.30

--- src/branch.js
+++ src/branch.js
@@ -4,28 +4,37 @@ import {markerPropertyName} from './branch-avoid-circular-dependency'
 
 export default (test, consequent, alternate = props => props) => {
   const ret = Component => {
-    const ConsequentAsComponent = flowMax(consequent, Component)
-    if (
-      ConsequentAsComponent.displayName == null ||
-      ConsequentAsComponent.displayName === 'ret'
-    )
-      ConsequentAsComponent.displayName = `branch(${
-        Component.displayName != null ? Component.displayName : ''
-      })`
-    const AlternateAsComponent = flowMax(alternate, Component)
-    if (
-      AlternateAsComponent.displayName == null ||
-      AlternateAsComponent.displayName === 'ret'
-    )
-      AlternateAsComponent.displayName = `branch(${
-        Component.displayName != null ? Component.displayName : ''
-      })`
-    return props =>
-      test(props) ? (
-        <ConsequentAsComponent {...props} />
-      ) : (
-        <AlternateAsComponent {...props} />
-      )
+    let ConsequentAsComponent
+    const createConsequent = () => {
+      const Consequent = flowMax(consequent, Component)
+      if (Consequent.displayName == null || Consequent.displayName === 'ret')
+        Consequent.displayName = `branch(${
+          Component.displayName != null ? Component.displayName : ''
+        })`
+      return Consequent
+    }
+    let AlternateAsComponent
+    const createAlternate = () => {
+      const Alternate = flowMax(alternate, Component)
+      if (Alternate.displayName == null || Alternate.displayName === 'ret')
+        Alternate.displayName = `branch(${
+          Component.displayName != null ? Component.displayName : ''
+        })`
+      return Alternate
+    }
+    return props => {
+      if (test(props)) {
+        if (ConsequentAsComponent == null) {
+          ConsequentAsComponent = createConsequent()
+        }
+        return <ConsequentAsComponent {...props} />
+      } else {
+        if (AlternateAsComponent == null) {
+          AlternateAsComponent = createAlternate()
+        }
+        return <AlternateAsComponent {...props} />
+      }
+    }
   }
   ret[markerPropertyName] = true
   return ret
